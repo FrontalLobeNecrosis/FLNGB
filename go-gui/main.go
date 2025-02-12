b96c0a3b0c0d59670a4bb97f72faede4c921f360
Persephone Wilder <shaneawilder@gmail.com>
2025-02-12 15:45:34 -0600

    MAde a basic gui with help of github copilot, will expand

--- go-gui/main.go
+++ go-gui/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	FLNGB := app.New()
+	gbWindow := FLNGB.NewWindow("FLNGB")
+
+	quitButton := widget.NewButton("Quit", func() {
+		FLNGB.Quit()
+	})
+	gbWindow.SetContent(container.NewVBox(
+		quitButton,
+	))
+
+	gbWindow.ShowAndRun()
+}
