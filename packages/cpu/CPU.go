e57a12cc897937df84f6de9fcb49cc077581b7d2
Persephone Wilder <shaneawilder@gmail.com>
2025-02-13 15:14:37 -0600

    Gave CPU struct registers

--- packages/cpu/CPU.go
+++ packages/cpu/CPU.go
@@ -1,4 +1,15 @@
 package GBCPU
 
 type CPU struct {
+	registerA     uint8
+	registerF     uint8
+	registerB     uint8
+	registerC     uint8
+	registerD     uint8
+	registerE     uint8
+	registerH     uint8
+	registerL     uint8
+	flagresgister uint8
+	registerSP    uint16
+	registerPC    uint16
 }
