d88bea1a23b9557769374f261ec3300eec937b8c
Persephone Wilder <shaneawilder@gmail.com>
2025-02-13 18:45:37 -0600

    Added cycle count to CPU struct and made a init function for CPU

--- packages/cpu/CPU.go
+++ packages/cpu/CPU.go
@@ -12,4 +12,22 @@ type CPU struct {
 	flagresgister uint8
 	registerSP    uint16
 	registerPC    uint16
+	cycles        uint32
+}
+
+func CPUinit() *CPU {
+	cpu := new(CPU)
+	cpu.registerA = 0
+	cpu.registerF = 0
+	cpu.registerB = 0
+	cpu.registerC = 0
+	cpu.registerD = 0
+	cpu.registerE = 0
+	cpu.registerH = 0
+	cpu.registerL = 0
+	cpu.flagresgister = 0b00000000
+	cpu.registerSP = 0x0100
+	cpu.registerPC = 0xFFFE
+	cpu.cycles = 0
+	return cpu
 }
