aeceb7fa55abeffcf5202b150e48ee08533ea949
Persephone Wilder <shaneawilder@gmail.com>
2025-02-14 04:46:35 -0600

    Made a struct to contain an array full of the function calls the opcodes would make

--- packages/cpu/Opcodes.go
+++ packages/cpu/Opcodes.go
@@ -1,11 +1,29 @@
 package GBCPU
 
-func readOpcode(opcode uint8) {
-	switch opcode {
+// This struct is to load functions from an array based on opcode
+type Opcode_function_loader struct {
+	eitghtBitFuncArray  [255]*func()
+	sixteenBitFuncArray [255]*func()
+}
 
-	case 0x00:
-		break
+/*
+LD loads a value from a register nn into a another register
+or immediate value n
+*/
+func LD(nn uint8, n uint8) uint8 {
+	n = nn
+	return n
+}
 
+/*
+Takes in an opcode and runs the function associated with that code
+param: an 8 bit or 16 bit value (16 bit has to begin at 0xCB00 and ends at 0xCBFF)
+*/
+func ReadOpcode(opcode uint16) {
+	loader := new(Opcode_function_loader)
+	if opcode > 255 {
+		loader.sixteenBitFuncArray[opcode-51968]
+	} else {
+		loader.eitghtBitFuncArray[opcode]
 	}
-
 }
