4300324fdb2f5941b3da8b2a8df6f7a6c3d1e40b
Persephone Wilder <shaneawilder@gmail.com>
2025-02-19 16:21:00 -0600

    Changed name of Opcodes_function_loader to Opscode_function_caller and gave detail to params for certain functions

--- packages/cpu/Opcodes.go
+++ packages/cpu/Opcodes.go
@@ -1,7 +1,7 @@
 package GBCPU
 
-// This struct is to load functions from an array based on opcode
-type Opcode_function_loader struct {
+// This struct is to call functions from an array based on opcode
+type Opcode_function_caller struct {
 	eightBitFuncArray   [255]func(uint8, uint8) uint8
 	eightbitparam1      [255]uint8
 	eightbitparam2      [255]uint8
@@ -10,88 +10,89 @@ type Opcode_function_loader struct {
 	sixteenbitparam2    [255]uint16
 }
 
-// Function makes an Opcode_function_loader and  takes a CPU and loades the loader
-// with all the functions and params that will be called by Opcodes
-func initLoader(cpu *CPU) *Opcode_function_loader {
-	loader := new(Opcode_function_loader)
+// Function makes an Opcode_function_caller and takes a CPU struct and loades the
+// caller with all the functions and params that will be called by Opcodes
+func initCaller(cpu *CPU) *Opcode_function_caller {
+	caller := new(Opcode_function_caller)
 
 	for i := 0; i <= 255; i++ {
 
 		if i < 30 && i%8 == 6 {
-			loader.eightBitFuncArray[i] = LDn
+			caller.eightBitFuncArray[i] = LDn
 		}
 
 		if (i >= 0x77 && i <= 0x7F) || (i >= 0x40 && i <= 0x75) || i == 36 {
 
-			loader.eightBitFuncArray[i] = LDr
+			caller.eightBitFuncArray[i] = LDr
 
 			if i >= 0x78 && i <= 0x7F {
-				loader.eightbitparam1[i] = cpu.registerA
+				caller.eightbitparam1[i] = cpu.registerA
 			} else if i >= 0x40 && i >= 0x47 {
-				loader.eightbitparam1[i] = cpu.registerB
+				caller.eightbitparam1[i] = cpu.registerB
 			} else if i >= 0x48 && i <= 0x4F {
-				loader.eightbitparam1[i] = cpu.registerC
+				caller.eightbitparam1[i] = cpu.registerC
 			} else if i >= 0x50 && i <= 0x57 {
-				loader.eightbitparam1[i] = cpu.registerD
+				caller.eightbitparam1[i] = cpu.registerD
 			} else if i >= 0x58 && i <= 0x5F {
-				loader.eightbitparam1[i] = cpu.registerE
+				caller.eightbitparam1[i] = cpu.registerE
 			} else if i >= 0x60 && i <= 0x67 {
-				loader.eightbitparam1[i] = cpu.registerH
+				caller.eightbitparam1[i] = cpu.registerH
 			} else if i >= 0x68 && i <= 0x6F {
-				loader.eightbitparam1[i] = cpu.registerL
+				caller.eightbitparam1[i] = cpu.registerL
 			} else if (i >= 0x70 && i <= 0x75) || i == 36 {
-				loader.eightbitparam1[i] = cpu.registerHL
+				caller.eightbitparam1[i] = cpu.registerHL
 			}
 
 			remainder := i % 8
 
 			switch remainder {
 			case 0:
-				loader.eightbitparam2[i] = cpu.registerB
+				caller.eightbitparam2[i] = cpu.registerB
 				break
 			case 1:
-				loader.eightbitparam2[i] = cpu.registerC
+				caller.eightbitparam2[i] = cpu.registerC
 				break
 			case 2:
-				loader.eightbitparam2[i] = cpu.registerD
+				caller.eightbitparam2[i] = cpu.registerD
 				break
 			case 3:
-				loader.eightbitparam2[i] = cpu.registerE
+				caller.eightbitparam2[i] = cpu.registerE
 				break
 			case 4:
-				loader.eightbitparam2[i] = cpu.registerH
+				caller.eightbitparam2[i] = cpu.registerH
 				break
 			case 5:
-				loader.eightbitparam2[i] = cpu.registerL
+				caller.eightbitparam2[i] = cpu.registerL
 				break
 			case 6:
 				if i == 36 {
 					// This is a temporary param for immediate value,
 					// replace registryA with that later
-					loader.eightbitparam2[i] = cpu.registerA
+					caller.eightbitparam2[i] = cpu.registerA
 				} else {
-					loader.eightbitparam2[i] = cpu.registerC
+					caller.eightbitparam2[i] = cpu.registerC
 				}
 				break
 			case 7:
-				loader.eightbitparam2[i] = cpu.registerA
+				caller.eightbitparam2[i] = cpu.registerA
 				break
 			}
 
 		}
 
 	}
-	return loader
+	return caller
 }
 
-func NewLoader(cpu *CPU) *Opcode_function_loader {
-	loader := initLoader(cpu)
-	return loader
+func NewCaller(cpu *CPU) *Opcode_function_caller {
+	caller := initCaller(cpu)
+	return caller
 }
 
 // LDn loads a value from a register nn into another register
 // or immediate value n
-
+// param: nn, a register to have a value read from
+// 		  n, a register or immediate value to have a value written to
 func LDn(nn uint8, n uint8) uint8 {
 	n = nn
 	return n
@@ -99,24 +100,26 @@ func LDn(nn uint8, n uint8) uint8 {
 
 // LDr loads a value from a register r2 into another register
 // or immediate value r1
+// param: r2, a register to have a value read from
+// 		  r1, a register or immediate value to have a value written to
 func LDr(r1 uint8, r2 uint8) uint8 {
 	r1 = r2
 	return 0
 }
 
-// Takes in an opcode and runs the function associated with that code
+// Takes in an opcode and runs the function with appropriate params associated with that code
 // param: an 8 bit or 16 bit value (16 bit has to begin at 0xCB00 and ends at 0xCBFF)
 func ReadOpcode(opcode uint16, cpu *CPU) {
-	loader := NewLoader(cpu)
+	caller := NewCaller(cpu)
 	if opcode > 255 {
-		function := loader.sixteenBitFuncArray[opcode-0xCB00]
-		first := loader.sixteenbitparam1[opcode-0xCB00]
-		second := loader.sixteenbitparam2[opcode-0xCB00]
+		function := caller.sixteenBitFuncArray[opcode-0xCB00]
+		first := caller.sixteenbitparam1[opcode-0xCB00]
+		second := caller.sixteenbitparam2[opcode-0xCB00]
 		function(first, second)
 	} else {
-		function := loader.eightBitFuncArray[opcode]
-		first := loader.eightbitparam1[opcode]
-		second := loader.eightbitparam2[opcode]
+		function := caller.eightBitFuncArray[opcode]
+		first := caller.eightbitparam1[opcode]
+		second := caller.eightbitparam2[opcode]
 		function(first, second)
 	}
 }
