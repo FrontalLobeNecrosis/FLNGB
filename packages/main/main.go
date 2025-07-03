ab4bf208dfa31aa61e9f555314d37a74988e3200
Persephone Wilder <shaneawilder@gmail.com>
2025-07-03 13:53:31 -0600

    Set up main.go to run the application loop and for testing

--- packages/main/main.go
+++ packages/main/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	GBCPU "github.com/FrontalLobeNecrosis/FLNGB/packages/cpu"
+	GBPPU "github.com/FrontalLobeNecrosis/FLNGB/packages/ppu"
+)
+
 func main() {
+	var quit bool = false
+	cpu := GBCPU.NewCPU()
+	memory := GBCPU.NewMemory()
+	var immediateValue uint16 = 0
+	var opcode uint16 = 0
+	functionCaller := GBCPU.NewCaller(cpu, memory, &immediateValue)
+	for !quit {
+		GBCPU.ReadOpcode(functionCaller, &opcode, cpu, memory)
+	}
 
 }
