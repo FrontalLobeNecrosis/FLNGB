401def451c4a4fe68aebbbb0d969d6bc9fe5c8c7
Persephone Wilder <shaneawilder@gmail.com>
2025-07-03 14:16:19 -0600

    Moved main.go out of packages folder

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	GBCPU "github.com/FrontalLobeNecrosis/FLNGB/packages/cpu"
-	GBPPU "github.com/FrontalLobeNecrosis/FLNGB/packages/ppu"
+	//GBPPU "github.com/FrontalLobeNecrosis/FLNGB/packages/ppu"
 )
 
 func main() {
@@ -13,7 +13,7 @@ func main() {
 	var opcode uint16 = 0
 	functionCaller := GBCPU.NewCaller(cpu, memory, &immediateValue)
 	for !quit {
-		GBCPU.ReadOpcode(functionCaller, &opcode, cpu, memory)
+		GBCPU.ReadOpcode(functionCaller, opcode, cpu, memory)
 	}
 
 }
