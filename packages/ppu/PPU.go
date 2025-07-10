c08db12c3deeba63ee60fb12ffb2ffd9f73a5fcc
Persephone Wilder <shaneawilder@gmail.com>
2025-07-10 15:43:56 -0600

    Added consts for VRAM info

--- packages/ppu/PPU.go
+++ packages/ppu/PPU.go
@@ -7,9 +7,27 @@ const (
 	light_grey
 	dark_grey
 	white
+	VRAM_START = 0x8000
+	VRAM_END   = 0x9FFF
+	VRAM_SIZE  = VRAM_END - VRAM_START + 1
 )
 
 type PPU struct {
-	size   uint16
-	pallet []colour
+	pallet [VRAM_END - VRAM_START + 1]colour
+}
+
+func initPPU(ppu *PPU) {
+	for i := 0; uint16(i) <= VRAM_SIZE; i++ {
+		ppu.pallet[i] = 0
+	}
+}
+
+func NewPPU() *PPU {
+	ppu := new(PPU)
+	initPPU(ppu)
+	return ppu
+}
+
+func ReadPallet(memory []uint8) {
+
 }
